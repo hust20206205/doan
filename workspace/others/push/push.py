f0e8aaa9d47e1df0b8c2248d11851b5da500409a
VuVanNghia <nghiavu2k2abc@gmail.com>
2023-12-26 22:08:17 +0700

    VuVanNghia20206205

--- workspace/others/push/push.py
+++ workspace/others/push/push.py
@@ -1,20 +1,21 @@
 import os
 from modules.MyGit import MyGit
 # from modules.MyFormat import MyFormat
-# from modules.MyView import MyView
+from modules.MyView import MyView
 
 message = "VuVanNghia20206205"
 
 git_folder =  os.path.join(os.getcwd(), '../../../')
+
+MyGit.chdir(git_folder)
+MyGit.add()
+MyGit.commit(message)
+
 # contents =  os.path.join(os.getcwd(), '../../../baocao/contents')
 # main =  os.path.join(os.getcwd(), '../../../baocao/main.tex')
 # init_path =  os.path.join(os.getcwd(), '../../../baocao/contents/start/init.sty')
 # workspace_path = os.path.join(os.getcwd(), '../../report.code-workspace')
 # gitignore_path = os.path.join(git_path, ".gitignore") 
-
-MyGit.chdir(git_folder)
-MyGit.add()
-MyGit.commit(message)
 
 # MyFormat.latex(contents)
 # MyFormat.latex(main)
@@ -23,10 +24,8 @@ MyGit.commit(message)
 # MyFormat.basic(init_path)
 # MyFormat.workspace(workspace_path)
 
-# MyView.CloseTab()
-# MyView.Target(2)
-# MyView.CloseTerminal()
-# MyView.CloseScrollBar()
-# # MyView.CollapseFolders()
-# # MyView.OpenGit()
-# MyView.OpenLatex()
+MyView.CloseTab()
+MyView.Target(2)
+MyView.CloseTerminal()
+MyView.CloseScrollBar()
+MyView.OpenLatex()
