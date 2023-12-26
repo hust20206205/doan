3d0f6555d337c3911642478b4245634ecf0cf6b1
VuVanNghia <nghiavu2k2abc@gmail.com>
2023-12-26 21:32:52 +0700

    1

---
+++ workspace/others/push/push.py
@@ -0,0 +1,32 @@
+import os
+from modules.MyGit import MyGit
+# from modules.MyFormat import MyFormat
+# from modules.MyView import MyView
+
+message = "VuVanNghia20206205"
+
+git =  os.path.join(os.getcwd(), '../../../')
+contents =  os.path.join(os.getcwd(), '../../../baocao/contents')
+main =  os.path.join(os.getcwd(), '../../../baocao/main.tex')
+# init_path =  os.path.join(os.getcwd(), '../../../baocao/contents/start/init.sty')
+# workspace_path = os.path.join(os.getcwd(), '../../report.code-workspace')
+# gitignore_path = os.path.join(git_path, ".gitignore") 
+
+MyGit.chdir(git)
+MyGit.add()
+MyGit.commit(message)
+
+# MyFormat.latex(contents)
+# MyFormat.latex(main)
+# MyFormat.markdown(git_path)
+# MyFormat.basic(gitignore_path)
+# MyFormat.basic(init_path)
+# MyFormat.workspace(workspace_path)
+
+MyView.CloseTab()
+MyView.Target(2)
+MyView.CloseTerminal()
+MyView.CloseScrollBar()
+# MyView.CollapseFolders()
+# MyView.OpenGit()
+MyView.OpenLatex()
