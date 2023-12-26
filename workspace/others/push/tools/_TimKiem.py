0c3c750f8f1bcf1dc06599c75a5d7826a2bc3f80
VuVanNghia <nghiavu2k2abc@gmail.com>
2023-12-26 22:51:18 +0700

    VuVanNghia20206205

--- workspace/others/push/tools/_TimKiem.py
+++ workspace/others/push/tools/_TimKiem.py
@@ -8,10 +8,12 @@ def TimKiem(root_dir, ext):
 
 
 folder = os.path.join(os.getcwd(), "../../")
+folder =   r"C:\Users\vvn20206205\Desktop\doan"
 print( folder)
 files = TimKiem(folder, '.srt')
 files = TimKiem(folder, '.tex')
 files = TimKiem(folder, '.md')
+files = TimKiem(folder, '.py')
 for f in files:
     print(f)
 
